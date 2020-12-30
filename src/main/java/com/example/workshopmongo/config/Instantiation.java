61fd2eb548ba6a334d36a38c4ba7e6b381a2cb4b
MarcosFornaroli <marcosfornaroli@gmail.com>
2020-12-29 21:37:13 -0300

    Consulta com vários critérios

--- src/main/java/com/example/workshopmongo/config/Instantiation.java
+++ src/main/java/com/example/workshopmongo/config/Instantiation.java
@@ -37,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
 		
-		Post post1 = new Post(null, sdf.parse("21/03/20108"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("21/03/20108"), "Bom dia", "Acordei feliz hoje.", new AuthorDTO(maria));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje.", new AuthorDTO(maria));
 
 		CommentDTO c1 = new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
 		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("21/03/2018"), new AuthorDTO(bob));
